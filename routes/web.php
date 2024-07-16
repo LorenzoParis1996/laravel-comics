ca033f4cb05c9289707d7c80b0b2b50f47ce0859
LorezoParis1996 <lorenzoparis86@live.it>
2024-07-16 12:22:31 +0200

    cambiato route per visualizzare pages.home

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
+/*Route::get('/home', function () {
     return view('pages.home');
-});
+});*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
